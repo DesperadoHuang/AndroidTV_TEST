2ad8611e1c71b992fe8aeacc1649f4fc327fbae4
Wilson Huang <hjn201@gmail.com>
2016-03-20 09:01:25 +0800

    no message

--- app/src/main/java/com/androidtv_test/model/MovieList.java
+++ app/src/main/java/com/androidtv_test/model/MovieList.java
@@ -32,7 +32,7 @@ public final class MovieList {
 
         //影片位置
         String videoUrl[] = {
-                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review.mp4",
+                "http://66.55.92.79:1935/app/_definst_/wjj-eras-channel/playlist.m3u8",
                 "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search.mp4",
                 "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue.mp4",
                 "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole.mp4",
